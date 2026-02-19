f6071dd1e7f964e5f3d153c1a7a4e4433d958384
Bakari Walton <113847561+CodeWithKarii@users.noreply.github.com>
2026-02-19 16:25:10 -0500

    Add files via upload
    first commit

---
+++ PAC2/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/go-home")
+def go_home():
+    return redirect(url_for("home"))
+
+@app.route("/go-portfolio")
+def go_portfolio():
+    return redirect(url_for("portfolio"))
+
+if __name__ == "__main__":
+     if __name__ == "__main__":
+        app.run()
